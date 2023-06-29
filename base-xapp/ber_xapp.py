f1214a1170fc8bfc9fde3aba047a0bad2ffae1f6
tugayozel <ozel_tugay@hotmail.com>
2023-06-29 13:45:53 +0200

    29June

--- base-xapp/ber_xapp.py
+++ base-xapp/ber_xapp.py
@@ -18,6 +18,39 @@ def send_indication_request():
     buf = master_mess.SerializeToString()
     xapp_control_ricbypass.send_to_socket(buf)
 
+def send_control_request(rnti, mcs_type):
+    print("Sending control message")
+    master_mess = RAN_message()
+    master_mess.msg_type = RAN_message_type.CONTROL
+    inner_mess = RAN_control_request()
+    
+    # ue list map entry
+    ue_list_control_element = RAN_param_map_entry()
+    ue_list_control_element.key = RAN_parameter.UE_LIST
+    
+    # ue list message 
+    ue_list_message = ue_list_m()
+    ue_list_message.connected_ues = 1 # this will not be processed by the gnb, it can be anything
+
+    # ue info message
+    ue_info_message = ue_info_m()
+    ue_info_message.rnti = rnti
+    ue_info_message.prop_1 = mcs_type
+    #ue_info_message.prop_2 = prop_2
+
+    # put info message into repeated field of ue list message
+    ue_list_message.ue_info.extend([ue_info_message])
+
+    # put ue_list_message into the value of the control map entry
+    ue_list_control_element.ue_list.CopyFrom(ue_list_message)
+
+    # finalize and send
+    inner_mess.target_param_map.extend([ue_list_control_element])
+    master_mess.ran_control_request.CopyFrom(inner_mess)
+    print(master_mess)
+    buf = master_mess.SerializeToString()
+    xapp_control_ricbypass.send_to_socket(buf)
+
 def control_function(ran_ind_resp):
     for ue_info in ran_ind_resp.param_map[1].ue_list.ue_info:
         print(ue_info)
--- base-xapp/control_xapp.py
+++ base-xapp/control_xapp.py
@@ -3,16 +3,32 @@ from e2sm_proto import *
 from time import sleep
 
 def main():
+    num_changes = int(input("How many you want to change? "))
 
-    rnti = input("Enter RNTI:")
-    rnti = int(rnti)
+    # ue list message 
+    ue_list_message = ue_list_m()
+    ue_list_message.connected_ues = 1 # this will not be processed by the gnb, it can be anything
 
-    decision = input("Change MCS? (y/n)")
-    if decision == "y":
-        prop_1 = RAN_mcs_type.SIXTYFOUR_QAM
-    else:
-        prop_1 = RAN_mcs_type.SIXTEEN_QAM
-    #prop_2 = float(input("Enter prop_2 (float)"))
+    for i in range(num_changes):
+
+        rnti = input("Enter RNTI:")
+        rnti = int(rnti)
+
+        decision = input("Change MCS? (y/n)")
+        if decision == "y":
+            prop_1 = RAN_mcs_type.SIXTYFOUR_QAM
+        else:
+            prop_1 = RAN_mcs_type.SIXTEEN_QAM
+        #prop_2 = float(input("Enter prop_2 (float)"))
+
+        # ue info message
+        ue_info_message = ue_info_m()
+        ue_info_message.rnti = rnti
+        ue_info_message.prop_1 = prop_1
+        #ue_info_message.prop_2 = prop_2
+
+        # put info message into repeated field of ue list message
+        ue_list_message.ue_info.extend([ue_info_message])
 
     print("Sending control message")
     master_mess = RAN_message()
@@ -22,19 +38,7 @@ def main():
     # ue list map entry
     ue_list_control_element = RAN_param_map_entry()
     ue_list_control_element.key = RAN_parameter.UE_LIST
-    
-    # ue list message 
-    ue_list_message = ue_list_m()
-    ue_list_message.connected_ues = 1 # this will not be processed by the gnb, it can be anything
 
-    # ue info message
-    ue_info_message = ue_info_m()
-    ue_info_message.rnti = rnti
-    ue_info_message.prop_1 = prop_1
-    #ue_info_message.prop_2 = prop_2
-
-    # put info message into repeated field of ue list message
-    ue_list_message.ue_info.extend([ue_info_message])
 
     # put ue_list_message into the value of the control map entry
     ue_list_control_element.ue_list.CopyFrom(ue_list_message)
